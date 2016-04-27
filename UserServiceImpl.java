d0d776f463a3e15ef571ce2bc1a7993ac1a2b7c2
SirMorti <ms.stelter@web.de>
2016-04-27 10:22:49 +0200

    User in JoinedChatroomListe/ keine Leeren Nachrichten mehr
    Wenn ein User einen Chatraum betritt wird dieser nun in die Sql Tabelle eingetragen. Geht ein User auf irgendeine andere Seite wird er aus dieser entfernt.

---
+++ UserServiceImpl.java
@@ -0,0 +1,132 @@
+package de.bonobo_talk.SpringMVCTest.service;
+
+import java.util.HashSet;
+import java.util.List;
+
+
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import de.bonobo_talk.SpringMVCTest.DAO.UserDAO;
+import de.bonobo_talk.SpringMVCTest.model.Chatroom;
+import de.bonobo_talk.SpringMVCTest.model.User;
+ 
+ 
+@Service("UserService")
+@Transactional
+public class UserServiceImpl implements UserService {
+ 
+    @Autowired
+    private UserDAO dao;
+     
+    public User findById(int id) {
+        return dao.findById(id);
+    }
+ 
+    public void saveOrUpdateUser(User User) {
+        dao.saveOrUpdateUser(User);
+    }
+ 
+    public void updateUser(User User) {
+        User entity = dao.findById(User.getId());
+        if(entity!=null){
+        	entity.setFirstname(User.getFirstname());
+        	entity.setLastname(User.getLastname());
+        	entity.setGender(User.getGender());
+        	entity.setDateOfBirth(User.getDateOfBirth());
+        	entity.setUserpicture(User.getUserpicture());
+        	entity.setFavouriteChatrooms(User.getFavouriteChatrooms());
+        }
+    }
+ 
+    @Override
+    public void deleteUser(String username){
+        dao.deleteUser(username);
+    }
+     
+
+ 
+    public List<User> getAllUser() {
+    	return dao.getAllUser();
+    }
+ 
+    public User findUserByUsername(String username) {
+    	return dao.findUserByUsername(username);
+    }
+
+	@Override
+	public void addFavouriteChatroom(int userID, Chatroom chatroom) {
+		// TODO Auto-generated method stub
+		User entity = dao.findById(userID);
+        if(entity!=null){
+        	Set<Chatroom> tmp = entity.getFavouriteChatrooms();
+        	tmp.add(chatroom);
+        	entity.setFavouriteChatrooms(tmp);
+        }
+	}
+	
+	@Override
+	public void removeFavouriteChatroom(int userID, Chatroom chatroom) {
+		// TODO Auto-generated method stub
+		User entity = dao.findById(userID);
+        if(entity!=null){
+        	Set<Chatroom> tmp = entity.getFavouriteChatrooms();
+        	tmp.remove(chatroom);
+        	entity.setFavouriteChatrooms(tmp);
+        }
+	}
+
+
+	@Override
+	public void joinChatroom(int userID, Chatroom chatroom) {
+		// TODO Auto-generated method stub
+		User entity = dao.findById(userID);
+        if(entity!=null){
+        	Set<Chatroom> tmp = entity.getJoinedChatrooms();
+        	tmp.add(chatroom);
+        	entity.setJoinedChatrooms(tmp);
+        }
+	}
+
+	@Override
+	public void leaveChatroom(int userID, Chatroom chatroom) {
+		// TODO Auto-generated method stub
+		User entity = dao.findById(userID);
+        if(entity!=null){
+        	Set<Chatroom> tmp = entity.getJoinedChatrooms();
+        	tmp.remove(chatroom);
+        	entity.setJoinedChatrooms(tmp);
+        }
+	}
+
+	@Override
+	public void deleteUser(int id) {
+		// TODO Auto-generated method stub
+		dao.deleteUser(id);
+	}
+
+	@Override
+	public Set<String> getAllUsernames() {
+		List<User> users = getAllUser();
+		Set<String> listUsernames = new HashSet<String>();
+		for (User user : users) 
+		{
+			listUsernames.add(user.getUsername());
+		}
+		return listUsernames;
+	}
+
+	@Override
+	public void leaveAllChatrooms(User user) {
+		User entity = dao.findById(user.getId());
+		if(entity != null)
+		{
+			entity.setJoinedChatrooms(new HashSet<Chatroom>());
+		}
+	}
+	
+}
---
+++ WebchatController.java
@@ -0,0 +1,178 @@
+package de.bonobo_talk.SpringMVCTest.web;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.hibernate.mapping.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.portlet.ModelAndView;
+
+import de.bonobo_talk.SpringMVCTest.model.Chatroom;
+import de.bonobo_talk.SpringMVCTest.model.User;
+import de.bonobo_talk.SpringMVCTest.service.ChatroomService;
+import de.bonobo_talk.SpringMVCTest.service.UserService;
+
+@Controller
+
+public class WebchatController 
+{
+		    @Autowired
+		    UserService service;   
+		    @Autowired
+		    MessageSource messageSource;
+		    @Autowired
+		    ChatroomService chatroomService;
+		 
+		    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+		    public String showStartsite(ModelMap model) {
+		 
+		        //List<User> Users = service.getAllUser();
+		        //model.addAttribute("Users", Users);
+		    	if(getPrincipal() != null)
+		    	{
+			    	service.leaveAllChatrooms(getPrincipal());
+		    		return "redirect:/startseite";
+		    	}
+		        return "index";
+		    }
+/*
+		    @RequestMapping(value = { "/list" }, method = RequestMethod.GET)
+		    public String listUsers(ModelMap model) {
+		 
+		        List<User> Users = service.getAllUser();
+		        model.addAttribute("Users", Users);
+		        return "allUsers";
+		    }
+		    
+*/
+		    @RequestMapping(value = { "/new" }, method = RequestMethod.GET)
+		    public String newUser(ModelMap model) {
+		        User User = new User();
+		        model.addAttribute("User", User);
+		        model.addAttribute("edit", false);
+		        return "registration";
+		    }
+
+		    @RequestMapping(value = { "/new" }, method = RequestMethod.POST)
+		    public String saveUser(@Valid User User, BindingResult result,
+		            ModelMap model) {
+		 
+		        if (result.hasErrors()) {
+		            return "registration";
+		        }
+		 
+
+		        if(service.findUserByUsername(User.getUsername())!= null){
+		            return "registration";
+		        }
+		         
+		        service.saveOrUpdateUser(User);
+		 
+		        model.addAttribute("success", "User " + User.getUsername() + " registered successfully");
+		        return "success";
+		    }
+		    
+		    @RequestMapping(value = { "/startseite" }, method = RequestMethod.GET)
+		    public String showStartseite(ModelMap model) {
+		        User user = getPrincipal();
+		        if(user == null)
+		        {
+		        	return "redirect:/index";
+		        }
+		        service.leaveAllChatrooms(user);
+		    	model.addAttribute("User", user);
+		        return "startseite";
+		    }
+		 		    
+		    @RequestMapping(value = { "/profile" }, method = RequestMethod.GET)
+		    public String showProfile(ModelMap model) {
+		    	User user = getPrincipal();
+		        if(user == null)
+		        {
+		        	return "redirect:/index";
+		        }
+		        service.leaveAllChatrooms(user);
+		    	model.addAttribute("User", user);
+		        return "profile";
+		    }
+		    
+		    @RequestMapping(value = { "/chatselect" }, method = RequestMethod.GET)
+		    public String selectChatroom(ModelMap model) {
+		    	User user = getPrincipal();
+		        if(user == null)
+		        {
+		        	return "redirect:/index";
+		        }
+		        service.leaveAllChatrooms(user);
+		    	model.addAttribute("User", user);
+		        model.addAttribute("Chatrooms", chatroomService.getAllChatrooms());
+		        model.addAttribute("Categories", chatroomService.getAllCategories());
+		        return "chatselect";
+		    }
+		    
+		    @RequestMapping(value = { "/joinChatroom" }, method = RequestMethod.POST)
+		    public String joinChatroom(@RequestParam("chatroomId") int chatroomId, ModelMap model) 
+		    {
+		    	User user = getPrincipal();
+		        if(user == null)
+		        {
+		        	return "redirect:/index";
+		        }
+		    	Chatroom currChatroom = chatroomService.findById(chatroomId);
+		    	if(currChatroom == null)
+		    	{
+		    		return "redirect:/chatselect";
+		    	}
+		        service.leaveAllChatrooms(user);
+		    	service.joinChatroom(user.getId(), currChatroom);
+		    	model.addAttribute("Chatroom", currChatroom);
+		    	model.addAttribute("User", user);
+		    	return "chat";
+		    }
+		    
+		    @RequestMapping(value = {"/faq"}, method = RequestMethod.GET)
+		    public String showFAQ(ModelMap model)
+		    {
+		    	User user = getPrincipal();
+		    	if(user != null)
+		    	{
+		    		service.leaveAllChatrooms(user);
+		    	}
+		    	model.addAttribute("User", user);
+		    	return "faq";
+		    }
+		   
+		    private User getPrincipal(){
+		        String userName = null;
+		        User user;
+		        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		        //TODO: müll nach username suchen
+		        if (principal instanceof UserDetails) {
+		            userName = ((UserDetails)principal).getUsername();
+		            user = service.findUserByUsername(userName);
+		            
+		        } else {
+		            userName = principal.toString();
+		            user = null;
+		        }
+		        
+		        return user;
+		    }
+}
